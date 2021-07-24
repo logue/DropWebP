c5798316ffd9e0fc337921d34abb268d312af7e5
Logue <logue@hotmail.co.jp>
2021-07-24 11:45:56 +0900

    ステータスバーを削除。 ブラウズボタンはフォルダ指定にした。

--- DropWebP/ViewModels/HomeContentViewModel.cs
+++ DropWebP/ViewModels/HomeContentViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using DropWebP.Interfaces;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
+using Prism.Regions;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 using Windows.Storage;
 using Windows.Storage.Pickers;
-using Windows.UI.Popups;
 using WinRT;
 
 namespace DropWebP.ViewModels
@@ -18,12 +26,43 @@ namespace DropWebP.ViewModels
     /// </summary>
     public class HomeContentViewModel : BindableBase
     {
+        private IEventAggregator eventAggregator;
+        private IRegionManager regionManager;
+
+        /// <summary>
+        /// MetroWindow
+        /// </summary>
+        public MetroWindow Shell { get; set; } = Application.Current.MainWindow as MetroWindow;
+
+        /// <summary>
+        /// MainWindowのCloseイベント
+        /// </summary>
+        public ReactiveCommand ClosedCommand { get; } = new ReactiveCommand();
+
+        /// <summary>
+        /// Disposeが必要な処理をまとめてやる
+        /// </summary>
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+
         /// <summary>
         /// Webのエンコーダー
         /// </summary>
         private IWebPService webPService;
 
-        // private IStatusBarViewModel statusBarViewModel;
+        /// <summary>
+        /// 表示するイメージのファイル名
+        /// </summary>
+        public ReactivePropertySlim<string> ViewImage { get; } = new ReactivePropertySlim<string>();
+
+        /// <summary>
+        /// ImageのPreviewDragOverイベントのコマンド
+        /// </summary>
+        public ReactiveCommand<DragEventArgs> PreviewDragOverCommand { get; } = new ReactiveCommand<DragEventArgs>();
+
+        /// <summary>
+        /// Imageのイベントのコマンド
+        /// </summary>
+        public ReactiveCommand<DragEventArgs> DropCommand { get; } = new ReactiveCommand<DragEventArgs>();
 
         /// <summary>
         /// ファイルブラウザボタンのコマンド
@@ -45,15 +84,76 @@ namespace DropWebP.ViewModels
         /// </summary>
         public string Message { get; set; } = "Drag and drop image file(s) to convert WebP.";
 
+
+        /// <summary>
+        /// ダイアログのインスタンス
+        /// </summary>
+        public IDialogCoordinator MahAppsDialogCoordinator { get; set; }
+
+        /// <summary>
+        /// ダイアログ設定
+        /// </summary>
+        private MetroDialogSettings metroDialogSettings;
+
         /// <summary>
         /// コンストラクタ
         /// </summary>
-        public HomeContentViewModel(IWebPService webPService)
+        /// /// <summary>コンストラクタ</summary>
+        /// <param name="regionManager">インジェクションするIRegionManager。</param>
+        public HomeContentViewModel(IEventAggregator eventAggregator, IRegionManager regionManager, IWebPService webPService)
         {
+
+            // ドラッグアンドドロップハンドラ
+            _ = ClosedCommand.Subscribe(Close).AddTo(Disposable);
+            _ = PreviewDragOverCommand.Subscribe(ImagePreviewDragOver).AddTo(Disposable);
+            _ = DropCommand.Subscribe(ImageDrop).AddTo(Disposable);
+
+            // Metroダイアログのデフォルト設定
+            metroDialogSettings = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "OK",
+                NegativeButtonText = "Cancel"
+            };
+
+            // フォルダ選択ボタンイベントハンドラ
             BrowseButtonCommand = new DelegateCommand(ExecuteBrowseButtonCommand);
 
+            this.eventAggregator = eventAggregator;
+            this.regionManager = regionManager;
             this.webPService = webPService;
-            // this.statusBarViewModel = statusBarViewModel;
+        }
+
+
+        /// <summary>
+        /// ウィンドウが閉じられるイベント
+        /// </summary>
+        private void Close()
+        {
+            Disposable.Dispose();
+        }
+
+        /// <summary>
+        /// ImageのPreviewDragOverイベントに対する処理
+        /// </summary>
+        /// <param name="e"></param>
+        private void ImagePreviewDragOver(DragEventArgs e)
+        {
+            // マウスカーソルをコピーにする。
+            e.Effects = DragDropEffects.Copy;
+            // ドラッグされてきたものがFileDrop形式の場合だけ、このイベントを処理済みにする。
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+
+        /// <summary>
+        /// ImageのDropイベントに対する処理
+        /// </summary>
+        /// <param name="e"></param>
+        private void ImageDrop(DragEventArgs e)
+        {
+            // ドロップされたものがFileDrop形式の場合は、各ファイルのパス文字列を文字列配列に格納する。
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            Convert(files);
         }
 
         /// <summary>
@@ -61,57 +161,85 @@ namespace DropWebP.ViewModels
         /// </summary>
         private async void ExecuteBrowseButtonCommand()
         {
+
+            List<string> fileTypes = new List<string>();
+
             // ダイアログを定義
-            FileOpenPicker picker = new FileOpenPicker()
+            FolderPicker picker = new FolderPicker()
             {
                 ViewMode = PickerViewMode.Thumbnail,
                 SuggestedStartLocation = PickerLocationId.PicturesLibrary,
-                FileTypeFilter = { ".jpg", ".jpeg", ".png", ".bmp" }
+                FileTypeFilter = { ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".jxr" }
             };
             // ウィンドウバンドルを取得
             IInitializeWithWindow withWindow = picker.As<IInitializeWithWindow>();
             withWindow.Initialize(new WindowInteropHelper(Application.Current.MainWindow).Handle);
 
             // ファイルダイアログを表示
-            StorageFile file = await picker.PickSingleFileAsync();
+            StorageFolder folder = await picker.PickSingleFolderAsync();
             // IReadOnlyList<StorageFile> files = await picker.PickMultipleFilesAsync();
 
-            if (file == null)
+            if (folder == null)
             {
                 return;
             }
 
-            webPService.ConvertWebP(file.Path, Properties.Settings.Default.Lossless ? -1 : Properties.Settings.Default.Quality);
-            MessageDialog dialog = new MessageDialog("Finish");
-            withWindow = dialog.As<IInitializeWithWindow>();
-            withWindow.Initialize(new WindowInteropHelper(Application.Current.MainWindow).Handle);
-            _ = await dialog.ShowAsync();
+            // Application now has read/write access to all contents in the picked folder
+            // (including other sub-folder contents)
+            Windows.Storage.AccessCache.StorageApplicationPermissions.
+            FutureAccessList.AddOrReplace("PickedFolderToken", folder);
 
-            /*
-            // ファイルの個数
-            int count = files.Count;
+            // ファイル一蘭
+            string[] files = Directory.GetFiles(folder.ToString(), "*.jpg, *.jpeg, *.png, *.bmp, *.gif, *.jxr", SearchOption.TopDirectoryOnly);
 
-            // ダイアログ
-            MessageDialog dialog;
-            if (count == 0)
+            // 変換処理
+            Convert(files);
+        }
+
+        /// <summary>
+        /// 変換処理
+        /// </summary>
+        /// <param name="files">変換対象のファイル</param>
+        private async void Convert(string[] files)
+        {
+            // 全ファイル数
+            int count = files.Length;
+
+            // プログレスコントローラー
+            ProgressDialogController controller = await Shell.ShowProgressAsync("Now converting", "Initializing....", false, metroDialogSettings);
+            controller.SetIndeterminate();
+            // キャンセルボタンが押されたときの設定
+            controller.Canceled += async (object sender, EventArgs e) =>
             {
-                // 選択されているファイルがない場合
-                dialog = new MessageDialog("Operation cancelled.");
-                return;
-            }
-            else
+                controller.Minimum = 0;
+                controller.Maximum = 0;
+                await controller.CloseAsync();
+            };
+            // プログレスダイアログの進捗情報の登録
+            controller.Minimum = 0;
+            controller.Maximum = count;
+
+            await Task.Run(() =>
             {
-                for (int i = 0; i <= count - 1; i++)
+                // キャンセル可能
+                controller.SetCancelable(true);
+                for (int i = 0; i <= count; i++)
                 {
-                    webPService.ConvertWebP(files[i].Path, Properties.Settings.Default.Lossless ? -1 : Properties.Settings.Default.Quality);
-                }
-                dialog = new MessageDialog("Finish");
-            }
-            withWindow = dialog.As<IInitializeWithWindow>();
-            withWindow.Initialize(new WindowInteropHelper(Application.Current.MainWindow).Handle);
+                    if (i == count)
+                    {
+                        controller.SetCancelable(false);
+                        break;
+                    }
+                    controller.SetProgress(i);
+                    controller.SetMessage(files[i]);
 
-            _ = dialog.ShowAsync();
-            */
+                    // 変換処理
+                    webPService.ConvertWebPAsync(files[i], Properties.Settings.Default.Lossless ? -1 : Properties.Settings.Default.Quality);
+                }
+            });
+            await controller.CloseAsync();
+
+            await Shell.ShowMessageAsync("Finish.", count.ToString() + " files are converted.");
         }
     }
 }
--- DropWebP/ViewModels/ShellWindowViewModel.cs
+++ DropWebP/ViewModels/ShellWindowViewModel.cs
@@ -1,18 +1,14 @@
 ﻿using DropWebP.Interfaces;
-using DropWebP.Services;
 using DropWebP.Views;
 using MahApps.Metro.Controls;
-using MahApps.Metro.Controls.Dialogs;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
-using System;
 using System.Diagnostics;
 using System.Reactive.Disposables;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace DropWebP.ViewModels
@@ -25,7 +21,7 @@ namespace DropWebP.ViewModels
         /// <summary>
         /// MetroWindow
         /// </summary>
-        public MetroWindow Shell { get; set; } = System.Windows.Application.Current.MainWindow as MetroWindow;
+        public MetroWindow Shell { get; set; } = Application.Current.MainWindow as MetroWindow;
 
         /// <summary>
         /// MainWindowのCloseイベント
@@ -38,29 +34,9 @@ namespace DropWebP.ViewModels
         private CompositeDisposable Disposable { get; } = new CompositeDisposable();
 
         /// <summary>
-        /// 表示するイメージのファイル名
-        /// </summary>
-        public ReactivePropertySlim<string> ViewImage { get; } = new ReactivePropertySlim<string>();
-
-        /// <summary>
-        /// ImageのPreviewDragOverイベントのコマンド
-        /// </summary>
-        public ReactiveCommand<DragEventArgs> PreviewDragOverCommand { get; } = new ReactiveCommand<DragEventArgs>();
-
-        /// <summary>
-        /// Imageのイベントのコマンド
-        /// </summary>
-        public ReactiveCommand<DragEventArgs> DropCommand { get; } = new ReactiveCommand<DragEventArgs>();
-
-        /// <summary>
         /// プログレスリングの表示制御
         /// </summary>
         public bool ProgressRing { get; set; } = false;
-
-        /// <summary>
-        /// ステータスバーのビューモデル
-        /// </summary>
-        // public IStatusBarViewModel statusBarViewModel;
 
         /// <summary>
         /// WebPサービス
@@ -82,34 +58,22 @@ namespace DropWebP.ViewModels
         /// </summary>
         public DelegateCommand ConfigButtonCommand { get; set; }
 
-        public IDialogCoordinator MahAppsDialogCoordinator { get; set; }
-
-        /// <summary>
-        /// プログレスダイアログ設定
-        /// </summary>
-        private MetroDialogSettings metroDialogSettings;
-
 
         /// <summary>コンストラクタ</summary>
         /// <param name="regionManager">インジェクションするIRegionManager。</param>
         public ShellWindowViewModel(IEventAggregator eventAggregator, IRegionManager regionManager, IWebPService webPService)
         {
+            // リージョン登録
             _ = regionManager.RegisterViewWithRegion("ContentRegion", typeof(HomeContent));
             _ = regionManager.RegisterViewWithRegion("FlyoutRegion", typeof(ConfigFlyout));
             _ = regionManager.RegisterViewWithRegion("AboutRegion", typeof(AboutDialog));
-            _ = regionManager.RegisterViewWithRegion("StatusBarRegion", typeof(StatusBar));
 
-            _ = PreviewDragOverCommand.Subscribe(ImagePreviewDragOver).AddTo(Disposable);
-            _ = DropCommand.Subscribe(ImageDrop).AddTo(Disposable);
+            // ドラッグアンドドロップハンドラ
             _ = ClosedCommand.Subscribe(Close).AddTo(Disposable);
 
-            metroDialogSettings = new MetroDialogSettings()
-            {
-                AffirmativeButtonText = "OK",
-                NegativeButtonText = "Cancel"
-            };
-
+            // アバウトボタンイベントハンドラ
             AboutButtonCommand = new DelegateCommand(ShowAboutDialog, CanClick);
+            // 設定ボタンイベントハンドラ
             ConfigButtonCommand = new DelegateCommand(ShowConfigFloyout, CanClick);
 
             this.eventAggregator = eventAggregator;
@@ -125,86 +89,6 @@ namespace DropWebP.ViewModels
             Disposable.Dispose();
         }
 
-        /// <summary>
-        /// ImageのPreviewDragOverイベントに対する処理
-        /// </summary>
-        /// <param name="e"></param>
-        private void ImagePreviewDragOver(DragEventArgs e)
-        {
-            // マウスカーソルをコピーにする。
-            e.Effects = DragDropEffects.Copy;
-            // ドラッグされてきたものがFileDrop形式の場合だけ、このイベントを処理済みにする。
-            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
-        }
-
-        /// <summary>
-        /// ImageのDropイベントに対する処理
-        /// </summary>
-        /// <param name="e"></param>
-        private async void ImageDrop(DragEventArgs e)
-        {
-            // ドロップされたものがFileDrop形式の場合は、各ファイルのパス文字列を文字列配列に格納する。
-            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-            // ファイルの個数
-            int count = files.Length;
-
-            StatusBarEvent s = new StatusBarEvent();
-
-            ProgressDialogController controller = await Shell.ShowProgressAsync("Now converting", "Converting an image file.", false, metroDialogSettings);
-            controller.Canceled += async (object sender, EventArgs e) =>
-            {
-                s.message = "Cancelled.";
-                controller.Minimum = 0;
-                controller.Maximum = 0;
-                await controller.CloseAsync();
-            };
-            controller.Minimum = 0;
-            controller.Maximum = count;
-
-            Debug.WriteLine(count);
-
-            await Task.Run(() =>
-            {
-                s.message = "Initializing...";
-                s.progressMaximum = count;
-                s.progress = 0;
-
-                // キャンセル可能
-                controller.SetCancelable(true);
-
-                // ステータスバーのビューモデルの更新
-                eventAggregator.GetEvent<StatusBarService>().Publish(s);
-                // 進捗リングを表示
-                ProgressRing = true;
-
-                for (int i = 0; i <= count; i++)
-                {
-                    if (i == count)
-                    {
-                        controller.SetCancelable(true);
-                        break;
-                    }
-                    Debug.WriteLine(files[i]);
-                    controller.SetProgress(i);
-                    controller.SetMessage(files[i]);
-
-                    s.message = files[i];
-                    s.progress = i;
-                    eventAggregator.GetEvent<StatusBarService>().Publish(s);
-                    webPService.ConvertWebPAsync(files[i], Properties.Settings.Default.Lossless ? -1 : Properties.Settings.Default.Quality);
-                }
-                // 進捗リングを非表示
-                ProgressRing = false;
-
-                s.progress = 0;
-                s.progressMaximum = 0;
-                s.message = "Finish.";
-                eventAggregator.GetEvent<StatusBarService>().Publish(s);
-            });
-            await controller.CloseAsync();
-
-        }
-
         private bool CanClick()
         {
             return true;
@@ -216,7 +100,6 @@ namespace DropWebP.ViewModels
         private void ShowAboutDialog()
         {
             Debug.WriteLine("アバウトクリック");
-            // eventAggregator.GetEvent<StatusBarService>().Publish("アバウトクリック");
         }
 
         /// <summary>
@@ -224,7 +107,6 @@ namespace DropWebP.ViewModels
         /// </summary>
         private void ShowConfigFloyout()
         {
-            // eventAggregator.GetEvent<StatusBarService>().Publish("設定ボタンクリック");
             Debug.WriteLine("設定ボタンクリック");
             regionManager.RequestNavigate("FlyoutRegion", "ConfigFlyout");
         }
