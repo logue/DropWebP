2b14975924751f06710125134a43089828522d26
Logue <logue@hotmail.co.jp>
2022-09-08 11:04:51 +0900

    例外ファイルのコミット忘れ

---
+++ DropWebP/Exceptions/WebPException.cs
@@ -0,0 +1,56 @@
+// -----------------------------------------------------------------------
+// <copyright file="WebPException.cs" company="Logue">
+// Copyright (c) 2021-2022 Masashi Yoshikawa All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Runtime.Serialization;
+
+namespace DropWebP.Exceptions
+{
+    /// <summary>
+    /// WebPService内で発生した例外
+    /// </summary>
+    internal class WebPException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebPException"/> class.
+        /// </summary>
+        /// <param name="e">Exception</param>
+        public WebPException(Exception e)
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebPException"/> class.
+        /// </summary>
+        /// <param name="message"><inheritdoc/></param>
+        public WebPException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebPException"/> class.
+        /// </summary>
+        /// <param name="message"><inheritdoc/></param>
+        /// <param name="innerException"><inheritdoc/></param>
+        public WebPException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebPException"/> class.
+        /// </summary>
+        /// <param name="info"><inheritdoc/></param>
+        /// <param name="context"><inheritdoc/></param>
+        protected WebPException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
