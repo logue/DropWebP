160e0a3d6e0aa20d2143276cdce7a0101da93e46
Logue <logue@hotmail.co.jp>
2025-10-30 22:14:53 +0900

    不要なインポートを削除

--- app/src/components/ContextMenu.vue
+++ app/src/components/ContextMenu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import { platform } from '@tauri-apps/plugin-os';
